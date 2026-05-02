5dbc260f8ea6bd5b9cbb5be1d2024a5ce9b6bad6
mcgooz <r.hughes001@gmail.com>
2026-05-02 12:41:56 +0200

    c2026

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className='fixed inset-x-0 bottom-0 text-center text-[0.7rem] mb-2'>
             <span className='inline-flex items-center gap-1'>
-                Barcelona, 2025 | Built with React 
+                © {new Date().getFullYear()} Richard Hughes | Built with React 
                 <a href="https://react.dev/" target="_blank" rel="noopener noreferrer">
                     <img className='w-3 brightness-200 hover:brightness-100 transition' src='assets/react_light.svg' alt="React logo" />
                 </a>
